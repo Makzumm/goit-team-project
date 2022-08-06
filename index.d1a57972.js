e507b7197d40f62f279e496302682540cade55d1
Makzumm <106184045+Makzumm@users.noreply.github.com>
2022-08-06 13:22:41 +0000

    Deploying to gh-pages from @ Makzumm/goit-team-project@0befdfbe8add76bf7d182cb23f0e88bdc4aa0f5f 🚀

---
+++ index.d1a57972.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-menu]"),t=document.querySelector("[data-menu-button]");t.addEventListener("click",(function(){var n="true"===t.getAttribute("aria-expanded")||!1;t.classList.toggle("is-open"),t.setAttribute("aria-expanded",!n),e.classList.toggle("is-open"),document.body.classList.toggle("menu-open")}))}();
+//# sourceMappingURL=index.d1a57972.js.map
