aa0014594ee1a2959a992be3f98e36a6c01d9380
Makzumm <106184045+Makzumm@users.noreply.github.com>
2022-08-08 10:26:29 +0000

    Deploying to gh-pages from @ Makzumm/goit-team-project@62e3a8679dba71faf3c50ffaf20881b9c0ca4e8c 🚀

---
+++ index.946f2621.js
@@ -0,0 +1,2 @@
+var buttons=document.querySelectorAll(".products-section__item button");function flipCard(t){t.currentTarget.closest(".products-section__item").classList.toggle("is-flipped")}buttons.forEach((function(t){return t.addEventListener("click",flipCard)}));
+//# sourceMappingURL=index.946f2621.js.map
--- index.b885a6fc.js
+++
@@ -1,2 +0,0 @@
-for(var _loop=function(i){var c=cards[i];c.addEventListener("click",(function(){c.classList.toggle("is-flipped")}))},cards=document.querySelectorAll(".products-section__item"),i=0;i<cards.length;i++)_loop(i);
-//# sourceMappingURL=index.b885a6fc.js.map
