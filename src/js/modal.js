9ba3494f126c99950929700c089f3c7616b1aa84
f0remangod <probill27@gmail.com>
2022-08-08 03:28:22 +0300

    modal-map
    додав модальне вікно "our places"

--- src/js/modal.js
+++ src/js/modal.js
@@ -8,8 +8,6 @@
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  refs.backdrop.addEventListener('click', logBackdropClick);
-
   function toggleModal() {
     refs.backdrop.classList.toggle('is-hidden');
     document.body.classList.toggle('modal-open');
