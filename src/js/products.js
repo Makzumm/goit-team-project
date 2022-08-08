d2abf3efc94bd81719bf89af54c05cd7a88df97f
LiubovErm <105315327+LiubovErm@users.noreply.github.com>
2022-08-08 13:14:38 +0300

    change script product and add button png

--- src/js/products.js
+++ src/js/products.js
@@ -8,10 +8,16 @@
 //   card.classList.toggle('is-flipped');
 // }
 
-const cards = document.querySelectorAll('.products-section__item');
-for (let i = 0; i < cards.length; i++) {
-  const card = cards[i];
-  card.addEventListener('click', function () {
-    card.classList.toggle('is-flipped');
-  });
+// const cards = document.querySelectorAll('.products-section__item');
+// for (let i = 0; i < cards.length; i++) {
+//   const card = cards[i];
+//   card.addEventListener('click', function () {
+//     card.classList.toggle('is-flipped');
+//   });
+// }
+
+const buttons = document.querySelectorAll('.products-section__item button');
+buttons.forEach((btn) => btn.addEventListener('click', flipCard));
+function flipCard({ currentTarget }) {
+  currentTarget.closest('.products-section__item').classList.toggle('is-flipped');
 }
