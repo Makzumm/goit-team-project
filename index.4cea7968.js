f15f53afdac0b97d6b76de9991820213db59c319
Makzumm <106184045+Makzumm@users.noreply.github.com>
2022-08-11 20:57:16 +0000

    Deploying to gh-pages from @ Makzumm/goit-team-project@0d01e6cb35ac2b041e172c1c26c8014959999a08 🚀

--- index.4cea7968.js
+++
@@ -1,2 +0,0 @@
-function scrollFunction(){document.body.scrollTop>1500||document.documentElement.scrollTop>1500?mybutton.style.opacity=1:mybutton.style.opacity=0}mybutton=document.getElementById("myBtn"),window.onscroll=function(){scrollFunction()};
-//# sourceMappingURL=index.4cea7968.js.map
