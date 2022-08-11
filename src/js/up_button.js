3c2ec1388e5ca010b961c1c1d4f14235e5232c93
SPoluliakh <S_Poluliakh@ukr.net>
2022-08-11 21:52:55 +0300

    fixed

--- src/js/up_button.js
+++ src/js/up_button.js
@@ -17,8 +17,8 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//   document.body.scrollTop = 0; // For Safari
+//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+// }
