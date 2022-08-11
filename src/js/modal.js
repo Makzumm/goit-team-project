e1d3d1457f3675418719ad5386b9a2aa5a78400b
f0remangod <probill27@gmail.com>
2022-08-11 11:37:58 +0300

    modals

--- src/js/modal.js
+++ src/js/modal.js
@@ -13,3 +13,35 @@
     document.body.classList.toggle('modal-open');
   }
 })();
+
+(() => {
+  const refs = {
+    openModalBtn2: document.querySelector('[data-modal-about-open]'),
+    closeModalBtn2: document.querySelector('[data-modal-about-close]'),
+    backdrop2: document.querySelector('[data-backdrop-about]'),
+  };
+
+  refs.openModalBtn2.addEventListener('click', toggleModal);
+  refs.closeModalBtn2.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.backdrop2.classList.toggle('is-hidden');
+    document.body.classList.toggle('modal-open');
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn1: document.querySelector('[data-modal-map-open]'),
+    closeModalBtn1: document.querySelector('[data-modal-map-close]'),
+    backdrop1: document.querySelector('[data-backdrop-modal]'),
+  };
+
+  refs.openModalBtn1.addEventListener('click', toggleModal);
+  refs.closeModalBtn1.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.backdrop1.classList.toggle('is-hidden');
+    document.body.classList.toggle('modal-open');
+  }
+})();
