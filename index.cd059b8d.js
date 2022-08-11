bb08d2554bf0dd2a3929ca1fc7f3e8f69a4458f0
Makzumm <106184045+Makzumm@users.noreply.github.com>
2022-08-11 19:59:10 +0000

    Deploying to gh-pages from @ Makzumm/goit-team-project@3e539ef8b704afcc667b5a1f3bd75df3a4a2bf8c 🚀

--- index.cd059b8d.js
+++
@@ -1,2 +0,0 @@
-function scrollFunction(){document.body.scrollTop>1500||document.documentElement.scrollTop>1500?mybutton.style.display="block":mybutton.style.display="none"}mybutton=document.getElementById("myBtn"),window.onscroll=function(){scrollFunction()};
-//# sourceMappingURL=index.cd059b8d.js.map
